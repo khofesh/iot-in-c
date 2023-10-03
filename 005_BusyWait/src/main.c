7a29f2391af167deaea5f55d874bdbbdfa8e2f42
fahmi ahmad <fahmiahmad@tutanota.com>
2023-10-03 12:52:33 +0100

    rename all the projects

---
+++ 005_BusyWait/src/main.c
@@ -0,0 +1,65 @@
+/**
+ * the delay time relates to the average pulse length for Pi 4:
+ * pulse length = delay + 68
+ *
+ * thi means, if you want to set a delay less then 70us or 90us, don't use
+ * nanosleep.
+ *
+ * void bcm2835_delayMicroseconds(time)
+ * uses a busy wait loop for times shorter than 450µs and a system delay
+ * like usleep or nanosleep for longer periods.
+ */
+#define _DEFAULT_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <bcm2835.h>
+#include <time.h>
+#include <signal.h>
+
+#define ever \
+    ;        \
+    ;
+
+void sigint_handler(int signal);
+
+int main(int argc, char **argv)
+{
+    // Set up the signal handler for Ctrl+C
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    {
+        perror("Error setting up signal handler");
+        return 1;
+    }
+
+    if (!bcm2835_init())
+    {
+        return (EXIT_FAILURE);
+    }
+
+    bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_07, BCM2835_GPIO_FSEL_OUTP);
+
+    struct timespec delay = {0, 10 * 1000};
+
+    for (ever)
+    {
+        bcm2835_gpio_set(RPI_BPLUS_GPIO_J8_07);
+        nanosleep(&delay, NULL);
+        bcm2835_gpio_clr(RPI_BPLUS_GPIO_J8_07);
+        nanosleep(&delay, NULL);
+    }
+
+    bcm2835_close();
+
+    return (EXIT_SUCCESS);
+}
+
+// Function to handle Ctrl+C (SIGINT) signal
+void sigint_handler(int signal)
+{
+    printf("\nCtrl+C received. Cleaning up and exiting...\n");
+    // Perform cleanup tasks here
+    // Close files, release memory, etc.
+    bcm2835_close();
+
+    exit(EXIT_SUCCESS); // Exit the program gracefully
+}
